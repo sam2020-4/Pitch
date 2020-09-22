45acf85131285f790efca87d2f89e8e1a1652d09
matata samuel <mattasamuel3@gmail.com>
2020-09-22 07:32:59 +0300

    Initializing flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -19,8 +19,29 @@ mail = Mail()
 
 def create_app(config_name):
 
-
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+   
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    # Initializing flask extensions
+    Bootstrap(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
