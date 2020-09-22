16defd47c23401490b3a18c76d1f61fa684936e3
matata samuel <mattasamuel3@gmail.com>
2020-09-22 08:55:50 +0300

    added new comment files

--- manage.py
+++ manage.py
@@ -1,10 +1,28 @@
 from app import create_app
 from flask_script import Manager,Server
-
-
-
+from  flask_migrate import Migrate, MigrateCommand
+from app.models import User, Pitches, Comments
 
 app = create_app('development')
+
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Pitches = Pitches, Comments= Comments)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
 if __name__ == '__main__':
