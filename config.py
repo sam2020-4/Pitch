8ea776c308696c2aada5ab382d46ba9afa45fc13
matata samuel <mattasamuel3@gmail.com>
2020-09-24 18:28:49 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ import os
 class Config:
 
     SECRET_KEY=os.environ.get('SECRET_KEY')
-   
+    # SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:sam2020@localhost/pitch'
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
